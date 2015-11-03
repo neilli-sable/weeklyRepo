a4482e032fc6599e99a22712f325aa92d912e074
Seiya Sakaguchi <omprand.serbei@gmail.com>
2015-11-03 17:01:38 +0900

    Improve layout.
    Expand and limit x char size on graph.

--- js/settings.js
+++ js/settings.js
@@ -2,5 +2,5 @@ var graphPlan = [];
 var graphDone = [];
 var graphNext = [];
 
-var margin = {top: 10, right: 10, bottom: 30, left: 120};
-var width = 600 - margin.left - margin.right;
+var margin = {top: 10, right: 10, bottom: 30, left: 230};
+var width = 720 - margin.left - margin.right;
